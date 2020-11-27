f3461c93256212e3c69adfa5ad97cdb5dda66827
Ismael Lourenço <ismaelourenco@msn.com>
2020-11-27 22:44:41 +0000

    AJPE

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int distance(int size, int path[size], int matrix[size][size])
 {
@@ -19,22 +21,73 @@ int distance(int size, int path[size], int matrix[size][size])
     return dist;
 }
 
+void swap(int size, int path[size]) {
+	int a = rand() % size;
+	int b = rand() % size;
+	int tmp = path[a];
+	path[a] = path[b];
+	path[b] = tmp;	
+}
+
+void shuffle(int *array, size_t n)
+{
+    if (n > 1) 
+    {
+        size_t i;
+        for (i = 0; i < n - 1; i++) 
+        {
+          size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
+          int t = array[j];
+          array[j] = array[i];
+          array[i] = t;
+        }
+    }
+}
+
 int main()
 {
 
+	srand(time(NULL));
+
     int size = 5;
     int matrix[5][5] = {
-        {0, 23, 10, 4, 1},
-        {23, 0, 9, 5, 4},
-        {10, 9, 0, 8, 2},
-        {4, 5, 8, 0, 11},
-        {1, 4, 2, 11, 0},
+	{0, 23, 10, 4, 1},
+	{23, 0, 9, 5, 4},
+	{10, 9, 0, 8, 2},
+	{4, 5, 8, 0, 11},
+	{1, 4, 2, 11, 0},
     };
 
-    int path[] = {5, 4, 2, 1, 3};
-
-    int dist = distance(size, path, matrix);
-
-	printf("\nDistance: %d\n\n", dist);
-return 0;
+    int path[size];
+	for(int i = 0; i<size; i++) {
+		path[i] = i + 1;
+	}
+	int bestPath[size];
+	for(int i = 0; i<size; i++) {
+		bestPath[i] = i + 1;
+	}
+	shuffle(path, size);
+	for(int i = 0; i<size; i++) {
+		printf(" %d ", path[i]);
+	}
+	int bestDist = 9999999;
+	
+	for (int i = 0; i < 10000; i++) {
+		swap(size, path);
+		int dist = distance(size, path, matrix);
+		if(dist < bestDist) {
+			bestDist = dist;
+			for (int z = 0; z < size; z++) {
+				bestPath[z] = path[z];			
+			}		
+		}
+		
+	} 
+	printf("\nDistance: %d\n\n", bestDist);
+	printf("Best Path -");	
+	for(int i = 0; i<size; i++) {
+		printf(" %d ", bestPath[i]);
+	}
+	printf("\n\n");	
+	return 0;
 }
